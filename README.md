0ee4610148c724232477d84093b57361af6a5c04
Martin Dahl <martin.dahl@gmail.com>
2020-04-29 11:40:38 +0200

    Minor cleanups

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ This package shows how to use r2r to create a minimal ros node.
 git clone [this repo]
 colcon build
 install/setup.sh
-ros2 run r2r_minimal_node r2r_minimal_node --ros-args -p param_key:=[hej,hopp] -p key2:=5.5 -r __ns:=/demo -r __node:=my_node
+ros2 run r2r_minimal_node r2r_minimal_node --ros-args -p param1:=[str1,str2] -p param2:=5.5 param3=true -r __ns:=/demo -r __node:=my_node
 ```
--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,7 @@ use r2r;
 use failure::Error;
 
 // try to run like this
-// ros2 run r2r_minimal_node r2r_minimal_node --ros-args -p param_key:=[hej,hopp] -p key2:=5.5 key2=true -r __ns:=/demo -r __node:=my_node
+// ros2 run r2r_minimal_node r2r_minimal_node --ros-args -p param1:=[str1,str2] -p param2:=5.5 param3=true -r __ns:=/demo -r __node:=my_node
 
 fn main() -> Result<(), Error> {
     let ctx = r2r::Context::create()?;
@@ -10,6 +10,9 @@ fn main() -> Result<(), Error> {
 
     println!("node name: {}", node.name()?);
     println!("node fully qualified name: {}", node.fully_qualified_name()?);
+
+    let msg = r2r::std_msgs::msg::String { data: "hello".into() };
+    println!("msg: {:?}", msg);
 
     println!("node parameters");
     node.params.iter().for_each(|(k,v)| {
