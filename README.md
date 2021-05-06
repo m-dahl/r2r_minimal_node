7a9cc30d66c331a8f806c22d2829c6d013606b08
Martin Dahl <martin.dahl@gmail.com>
2021-05-06 22:25:26 +0200

    use new r2r env vars and separate cmake logic into reusable file.

--- README.md
+++ README.md
@@ -3,7 +3,10 @@
 This package shows how to use r2r to create a minimal ros node.
 
 ``` sh
+mkdir src
+cd src
 git clone [this repo]
+cd ..
 colcon build
 install/setup.sh
 ros2 run r2r_minimal_node r2r_minimal_node --ros-args -p param1:=[str1,str2] -p param2:=5.5 param3=true -r __ns:=/demo -r __node:=my_node
---
+++ r2r_cargo.cmake
@@ -0,0 +1,71 @@
+# the recursive dependencies already defined don't seem to include all
+# packages. sigh.  so here we traverse the dependencies manually
+# instead. we also keep track of which packages that contain idl files.
+function(get_idl_deps OUT_INC OUT_LIB OUT_PKGS PKG)
+    find_package(${PKG} REQUIRED)
+    list(APPEND VISITED_TARGETS ${PKG})
+
+    set(INCS "${${PKG}_INCLUDE_DIRS}")
+    set(LIBS "${${PKG}_LIBRARIES}")
+    set(PKGS "")
+    list(APPEND PKGS ${PKG})
+    if(DEFINED ${PKG}_IDL_FILES)
+      list(APPEND PKGS ${PKG})
+    endif()
+    foreach(LIB ${${PKG}_DEPENDENCIES})
+        list(FIND VISITED_TARGETS ${LIB} VISITED)
+        if (${VISITED} EQUAL -1)
+            get_idl_deps(NEW_INCS NEW_LIBS NEW_PKGS ${LIB})
+            list(APPEND INCS ${NEW_INCS})
+            list(APPEND LIBS ${NEW_LIBS})
+            list(APPEND PKGS ${NEW_PKGS})
+            list(REMOVE_DUPLICATES INCS)
+            list(REMOVE_DUPLICATES LIBS)
+            list(REMOVE_DUPLICATES PKGS)
+        endif()
+    endforeach()
+    set(VISITED_TARGETS ${VISITED_TARGETS} PARENT_SCOPE)
+    set(${OUT_INC} ${INCS} PARENT_SCOPE)
+    set(${OUT_LIB} ${LIBS} PARENT_SCOPE)
+    set(${OUT_PKGS} ${PKGS} PARENT_SCOPE)
+endfunction()
+
+function(r2r_cargo)
+  foreach(f ${ARGN})
+    find_package(${f} REQUIRED)
+    set(REC_INC "")
+    set(REC_LIB "")
+    set(REC_IDL_PKGS "")
+    get_idl_deps(REC_INC REC_LIB REC_IDL_PKGS ${f})
+    list(APPEND CMAKE_INCLUDE_DIRS ${REC_INC})
+    list(APPEND CMAKE_LIBRARIES ${REC_LIB})
+    list(APPEND CMAKE_IDL_PACKAGES "${REC_IDL_PKGS}")
+  endforeach()
+  list(REMOVE_DUPLICATES CMAKE_INCLUDE_DIRS)
+  string (REPLACE ";" ":" CMAKE_INCLUDE_DIRS_STR "${CMAKE_INCLUDE_DIRS}")
+  set(ENV{CMAKE_INCLUDE_DIRS} ${CMAKE_INCLUDE_DIRS_STR})
+  list(REMOVE_DUPLICATES CMAKE_LIBRARIES)
+  string (REPLACE ":" "+" CMAKE_LIBRARIES_STR "${CMAKE_LIBRARIES}")
+  string (REPLACE ";" ":" CMAKE_LIBRARIES_STR "${CMAKE_LIBRARIES_STR}")
+  set(ENV{CMAKE_LIBRARIES} "${CMAKE_LIBRARIES_STR}")
+  list(REMOVE_DUPLICATES CMAKE_IDL_PACKAGES)
+  string (REPLACE ";" ":" CMAKE_IDL_PACKAGES_STR "${CMAKE_IDL_PACKAGES}")
+  set(ENV{CMAKE_IDL_PACKAGES} ${CMAKE_IDL_PACKAGES_STR})
+
+  # this works fine except that it will be called both on make all and again on make install...
+  option(CARGO_CLEAN "Invoke cargo clean before building" OFF)
+  if(CARGO_CLEAN)
+        add_custom_target(dummy_target ALL
+              COMMAND "cmake" "-E" "env" "cargo" "clean"
+              COMMAND "cmake" "-E" "env" "CMAKE_INCLUDE_DIRS=$ENV{CMAKE_INCLUDE_DIRS}" "CMAKE_LIBRARIES=$ENV{CMAKE_LIBRARIES}" "CMAKE_IDL_PACKAGES=$ENV{CMAKE_IDL_PACKAGES}" "cargo" "build" "--release"
+              WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+              )
+  else()
+          add_custom_target(dummy_target ALL
+              COMMAND "cmake" "-E" "env" "CMAKE_INCLUDE_DIRS=$ENV{CMAKE_INCLUDE_DIRS}" "CMAKE_LIBRARIES=$ENV{CMAKE_LIBRARIES}" "CMAKE_IDL_PACKAGES=$ENV{CMAKE_IDL_PACKAGES}" "cargo" "build" "--release"
+             WORKING_DIRECTORY ${CMAKE_SOURCE_DIR}
+              )
+  endif(CARGO_CLEAN)
+  unset(CARGO_CLEAN CACHE)
+
+endfunction()
