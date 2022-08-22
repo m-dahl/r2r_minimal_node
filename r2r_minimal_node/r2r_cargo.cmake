f15e51f8c8aaa68a38c12f2701c1f5b65d2e9294
Martin Dahl <martin.dahl@gmail.com>
2022-08-22 12:14:40 +0200

    cargo update + update to r2r 0.63 + config.toml example.

--- r2r_minimal_node/r2r_cargo.cmake
+++ r2r_minimal_node/r2r_cargo.cmake
@@ -1,34 +1,43 @@
+#
+# For r2r 0.6.3.
+#
+# cmake code for simple colcon integration.
+# See https://github.com/m-dahl/r2r_minimal_node/
+# for an example of how to use it to build with colcon.
+#
+
 # the "recursive" dependencies already defined don't seem to include all
 # packages. sigh. so here we traverse the dependencies manually
 # instead. we also keep track of which packages that contain idl files.
-function(get_idl_deps OUT_INC OUT_LIB OUT_PKGS PKG)
+function(get_idl_deps OUT_INC OUT_LIB OUT_PKG_DIRS PKG)
     find_package(${PKG} REQUIRED)
     list(APPEND VISITED_TARGETS ${PKG})
 
     set(INCS "${${PKG}_INCLUDE_DIRS}")
     set(LIBS "${${PKG}_LIBRARIES}")
 
-    set(PKGS "")
-    list(APPEND PKGS ${PKG})
+    # only keep track of packages that include idl files
+    set(PKG_DIRS "")
     if(DEFINED ${PKG}_IDL_FILES)
-      list(APPEND PKGS ${PKG})
+        list(APPEND PKG_DIRS ${${PKG}_DIR})
     endif()
+
     foreach(LIB ${${PKG}_DEPENDENCIES})
         list(FIND VISITED_TARGETS ${LIB} VISITED)
         if (${VISITED} EQUAL -1)
-            get_idl_deps(NEW_INCS NEW_LIBS NEW_PKGS ${LIB})
+            get_idl_deps(NEW_INCS NEW_LIBS NEW_PKG_DIRS ${LIB})
             list(APPEND INCS ${NEW_INCS})
             list(APPEND LIBS ${NEW_LIBS})
-            list(APPEND PKGS ${NEW_PKGS})
+            list(APPEND PKG_DIRS ${NEW_PKG_DIRS})
             list(REMOVE_DUPLICATES INCS)
             list(REMOVE_DUPLICATES LIBS)
-            list(REMOVE_DUPLICATES PKGS)
+            list(REMOVE_DUPLICATES PKG_DIRS)
         endif()
     endforeach()
     set(VISITED_TARGETS ${VISITED_TARGETS} PARENT_SCOPE)
     set(${OUT_INC} ${INCS} PARENT_SCOPE)
     set(${OUT_LIB} ${LIBS} PARENT_SCOPE)
-    set(${OUT_PKGS} ${PKGS} PARENT_SCOPE)
+    set(${OUT_PKG_DIRS} ${PKG_DIRS} PARENT_SCOPE)
 endfunction()
 
 function(r2r_cargo)
@@ -36,11 +45,11 @@ function(r2r_cargo)
     find_package(${f} REQUIRED)
     set(REC_INC "")
     set(REC_LIB "")
-    set(REC_IDL_PKGS "")
-    get_idl_deps(REC_INC REC_LIB REC_IDL_PKGS ${f})
+    set(REC_PKG_DIRS "")
+    get_idl_deps(REC_INC REC_LIB REC_PKG_DIRS ${f})
     list(APPEND CMAKE_INCLUDE_DIRS ${REC_INC})
     list(APPEND CMAKE_LIBRARIES ${REC_LIB})
-    list(APPEND CMAKE_IDL_PACKAGES "${REC_IDL_PKGS}")
+    list(APPEND CMAKE_IDL_PACKAGES "${REC_PKG_DIRS}")
   endforeach()
   list(REMOVE_DUPLICATES CMAKE_INCLUDE_DIRS)
   string (REPLACE ";" ":" CMAKE_INCLUDE_DIRS_STR "${CMAKE_INCLUDE_DIRS}")
@@ -64,6 +73,7 @@ function(r2r_cargo)
   string (REPLACE ":" "+" CMAKE_LIBRARIES_STR "${CMAKE_LIBRARIES}")
   string (REPLACE ";" ":" CMAKE_LIBRARIES_STR "${CMAKE_LIBRARIES_STR}")
   set(ENV{CMAKE_LIBRARIES} "${CMAKE_LIBRARIES_STR}")
+
   list(REMOVE_DUPLICATES CMAKE_IDL_PACKAGES)
   string (REPLACE ";" ":" CMAKE_IDL_PACKAGES_STR "${CMAKE_IDL_PACKAGES}")
   set(ENV{CMAKE_IDL_PACKAGES} ${CMAKE_IDL_PACKAGES_STR})
